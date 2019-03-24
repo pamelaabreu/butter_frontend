8e8fa8b2768db9214303d64d9e5bed1e88d052fb
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-24 11:03:45 -0400

    Set up AuthContext Provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Firebase from 'firebase';
+
+// ---- Context
+import AuthContext from './contexts/auth';
 
 class App extends Component {
+  state = {
+    user: null
+  }
+
+  componentDidMount() {
+    this.unsubscribe = Firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        this.setState({user});
+      }
+      else {
+        this.setState({user: null});
+      }
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
   render() {
     return (
-      <>
+      
+      <AuthContext.Provider value={this.state.user}>
         <h1>HELLO BITCH WASSUs</h1>
-      </>
+      </AuthContext.Provider>
+        
     );
   }
 }
