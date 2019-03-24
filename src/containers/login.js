ba2d74b3b251a2fbdb6fd79c9f27001723aa02e4
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-24 11:39:17 -0400

    Imported AuthContext

--- src/containers/login.js
+++ src/containers/login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import firebase from '../firebase';
-import AuthContext from '../context/auth';
+import AuthContext from '../contexts/auth';
 import { Redirect } from 'react-router-dom';
 
 export default class Login extends React.Component {
