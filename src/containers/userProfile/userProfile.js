551ead9081e73a53f2e0f19ef0228d958c7ef026
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-29 17:38:22 -0400

    Created auth and redirect if user is not logged in

--- src/containers/userProfile/userProfile.js
+++ src/containers/userProfile/userProfile.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import AuthContext from '../../contexts/auth';
 
 export default class UserProfile extends React.Component {
     render () {
         return (
-            <h1>Hello User Page!</h1>
+            <AuthContext.Consumer>
+                {
+                    user => user ? <>
+                        <div style={{marginTop:'100px'}}>
+                            <h2>Hello, {user.email}</h2>
+                            <h4>With ID:{user.uid} </h4>
+                            <Link style={{border:'2px solid rebeccapurple', padding:'20px'}} to='/logout'>Logout</Link> 
+                        </div>
+                    </>
+                    : 
+                    <Redirect to='/'/>
+                }
+            </AuthContext.Consumer>
         )
     }
 }
