a0b56e2e00613e613f3df8a3cc5a14be09161b7d
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-29 18:19:42 -0400

    Created another div for bacnkground img

--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -7,15 +7,17 @@ export default class Home extends React.Component {
   
   render() {
     const welcome = <>
-      <div>
-        <h1>butter</h1>
+      <div className='homeBackground'>
         <div>
-          <p>
-            A place to safely share open and honest confessions.
-            Sign up if you want to hear and speak about sex, shame, and insecurities.
-          </p>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/login">Login</Link>
+          <h1>butter</h1>
+          <div>
+            <p>
+              A place to safely share open and honest confessions.
+              Sign up if you want to hear and speak about sex, shame, and insecurities.
+            </p>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/login">Login</Link>
+          </div>
         </div>
       </div>
     </>
