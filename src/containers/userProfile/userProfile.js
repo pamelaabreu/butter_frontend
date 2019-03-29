fd847a6a2adeee473bcf662d6ef55bc7dc2a27ee
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-29 17:23:42 -0400

    Imported Redirect and AuthContext

--- src/containers/userProfile/userProfile.js
+++ src/containers/userProfile/userProfile.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import AuthContext from '../../contexts/auth';
 
 export default class UserProfile extends React.Component {
     render () {
