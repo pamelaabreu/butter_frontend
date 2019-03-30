718b9a1c22628bac6ff3f716ec1822d789b65cf5
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-30 01:39:30 -0400

    Added styling for the signup component

--- src/containers/signup/signup.js
+++ src/containers/signup/signup.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import firebase from '../../firebase';
 import AuthContext from '../../contexts/auth';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
+import './signup.css'
 
 export default class Signup extends React.Component {
 
   state = {
+    firstname: '',
+    username: '',
+    birthday:'',
     email: '',
     password: '',
+    joiningReason: '',
     error: ''
   }
 
@@ -30,23 +35,45 @@ export default class Signup extends React.Component {
   }
 
   render() {
-    const { email, password, error } = this.state;
-    const displayError = error === '' ? '' : <p role="alert">{error}</p>
-    const displayForm = <>
-        <h1>Sign Up</h1>
+    const { email, password, error, firstname, username, birthday, joiningReason } = this.state;
+    const displayError = error === '' ? '' : <p className='loginError signupError' role="alert">{error}</p>
+    const displayForm = <div className='loginBackground'>
+      <div className='loginBox'>
+        <div className='loginLeft'>
+          <h1 className='loginTitle'>Signup</h1>
+        </div>
+        <div className='loginRight signupRight'>
+          <div className='loginFlex'>
+            <label className='signupInputTitle'>Name</label>
+            <input className='signupInput' placeholder="Enter name" value={firstname} name='firstname' onChange={this.handleChange} />
+          </div>
+          <div className='loginFlex'>
+            <label className='signupInputTitle'>Username</label>
+            <input className='signupInput' placeholder="Enter username" value={username} name='username' onChange={this.handleChange} />
+          </div>
+          <div className='loginFlex'>
+            <label className='signupInputTitle'>Email</label>
+            <input className='signupInput' placeholder="Enter email" value={email} name='email' onChange={this.handleChange} />
+          </div>
+          <div className='loginFlex'>
+            <label className='signupInputTitle'>Password</label>
+            <input className='signupInput' placeholder="Enter password" value={password} name='password' onChange={this.handleChange} />
+          </div>
+          <div className='loginFlex'>
+            <label className='signupInputTitle'>Birthday</label>
+            <input className='signupInput' placeholder="Enter birthday" value={birthday} name='birthday' onChange={this.handleChange} />
+          </div>
+          <div className='loginFlex'>
+            <label className='signupInputTitle'>Why are you joining?</label>
+            <input className='signupInput' placeholder="Reason" value={joiningReason} name='joiningReason' onChange={this.handleChange} />
+          </div>
+          <button type="submit" className='signupButton homeSignUpButtonText homeSignUpButton' onClick={this.handleSubmit}>Sign Up</button>
+        </div>
+
         {displayError}
-        <form>
-            <div className="form-group">
-                <label>Email</label>
-                <input type="email" placeholder="Enter email" name="email" value={email} onChange={this.handleChange} />
-            </div>
-            <div className="form-group">
-                <label>Password</label>
-                <input type="password" placeholder="Password" value={password} name="password" onChange={this.handleChange} />
-            </div>
-            <button type="submit" onClick={this.handleSubmit}>Sign Up</button>
-        </form>
-  </>
+        <Link className='loginHomeButton' to='/'>Home</Link>  
+      </div>
+  </div>
 
     return (
       <AuthContext.Consumer>
