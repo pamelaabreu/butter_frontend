bed47043f66dcac75b118f7680773640ee6d91d3
Pamela Abreu <43766342+pamelaabreu@users.noreply.github.com>
2019-03-31 19:14:58 -0400

    Search JSX Styling and Throw Error for Required Fields (#13)
    * Added validation code for handleChange
    
    * Changed left position style on homeLoginBox
    
    * Updated love_email.png
    
    * Added another gradient color to transition into
    
    * Added function to show error if required input fields are filled in
    
    * Added media queries to make loginBox responsive
    
    * Organized JSX code
    
    * Added signUp styling

--- src/containers/signup/signup.js
+++ src/containers/signup/signup.js
@@ -2,78 +2,122 @@ import React from 'react';
 import firebase from '../../firebase';
 import AuthContext from '../../contexts/auth';
 import { Redirect, Link } from 'react-router-dom';
-import './signup.css'
+import './signup.css';
 
 export default class Signup extends React.Component {
 
   state = {
     firstname: '',
     username: '',
-    birthday:'',
+    birthday: '',
     email: '',
     password: '',
     joiningReason: '',
+    firebaseError: '',
     error: ''
   }
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  validateForms = () => {
+    const { firstname, username, birthday } = this.state;
+
+    return firstname.length > 0 && username.length > 0 && birthday.length > 0;
   }
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value.trim() })
 
   handleSubmit = (e) => {
     e.preventDefault();
 
-    const { email, password } = this.state;
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then((response) => {
-        console.log('Returns: ', response);
-      })
-      .catch(err => {
-        const { message } = err;
-        this.setState({ error: message });
-      })
+    if(this.validateForms()){
+      const { email, password } = this.state;
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((response) => {
+          console.log('Returns: ', response);
+          //Promise function to upload file
+          //Return the promise
+          //Then url promise axios request
+          // pass token through header
+        })
+        .catch(err => {
+          const { message } = err;
+          this.setState({ firebaseError: message });
+        })
+
+    } else {
+      this.setState({ error: 'Please fill out all required fields.' })
+    }
+
   }
 
   render() {
-    const { email, password, error, firstname, username, birthday, joiningReason } = this.state;
-    const displayError = error === '' ? '' : <p className='loginError signupError' role="alert">{error}</p>
+
+    const { error, firebaseError } = this.state;
+
+    // JSX DISPLAY FORM 
+    const displayFirebaseError = firebaseError === '' ? null : <p className='loginError signupError' role="alert">{firebaseError}</p>
+    const displayRequiredError = error === '' ? null : <p className='loginError signupError' role="alert">{error}</p>
+    const displayRequiredInputs = ["First Name", "Birthday", "Email", "Username", "Password", ].map((e, i) => {
+      if(e === 'First Name'){
+        return <div key={i} className='signupFlex'>
+          <label className='signupInputTitle'>
+          <span className='signupRequired'>* </span> 
+          {e}
+        </label>
+        <input className='signupInput' type='text' name='firstname' onChange={this.handleChange} />
+      </div>
+      } 
+      else if(e === 'Birthday'){
+        return <div key={i} className='signupFlex'>
+          <label className='signupInputTitle'>
+          <span className='signupRequired'>* </span> 
+          {e}
+        </label>
+        <input className='signupInput' type='date' name='birthday' onChange={this.handleChange} />
+      </div>
+      } 
+      else if (e === 'Password'){
+        return <div key={i} className='signupFlex'>
+          <label className='signupInputTitle'>
+          <span className='signupRequired'>* </span> 
+          {e}
+        </label>
+        <input className='signupInput' type='password' name='password' onChange={this.handleChange} />
+      </div>
+      } 
+      else return (
+        <div key={i} className='signupFlex'> 
+            <label className='signupInputTitle'>
+              <span className='signupRequired'>* </span> 
+              {e}
+            </label>
+            <input className='signupInput' name={e.toLowerCase()} onChange={this.handleChange} />
+        </div>
+      );
+    });
+    const displayJoiningInput = <div className='signupFlex'>
+      <label className='signupInputTitle'>Why are you joining?</label>
+      <input className='signupInput' name='joiningReason' onChange={this.handleChange} />
+    </div>
+
     const displayForm = <div className='loginBackground'>
-      <div className='loginBox'>
-        <div className='loginLeft'>
+      <Link className='loginHomeButton' to='/'>Home</Link>
+
+      <div className='loginBox signupBox'>
+        <div className='signupLeft'>
           <h1 className='loginTitle'>Signup</h1>
         </div>
-        <div className='loginRight signupRight'>
-          <div className='loginFlex'>
-            <label className='signupInputTitle'>Name</label>
-            <input className='signupInput' placeholder="Enter name" value={firstname} name='firstname' onChange={this.handleChange} />
-          </div>
-          <div className='loginFlex'>
-            <label className='signupInputTitle'>Username</label>
-            <input className='signupInput' placeholder="Enter username" value={username} name='username' onChange={this.handleChange} />
-          </div>
-          <div className='loginFlex'>
-            <label className='signupInputTitle'>Email</label>
-            <input className='signupInput' placeholder="Enter email" value={email} name='email' onChange={this.handleChange} />
-          </div>
-          <div className='loginFlex'>
-            <label className='signupInputTitle'>Password</label>
-            <input className='signupInput' placeholder="Enter password" value={password} name='password' onChange={this.handleChange} />
-          </div>
-          <div className='loginFlex'>
-            <label className='signupInputTitle'>Birthday</label>
-            <input className='signupInput' placeholder="Enter birthday" value={birthday} name='birthday' onChange={this.handleChange} />
-          </div>
-          <div className='loginFlex'>
-            <label className='signupInputTitle'>Why are you joining?</label>
-            <input className='signupInput' placeholder="Reason" value={joiningReason} name='joiningReason' onChange={this.handleChange} />
-          </div>
-          <button type="submit" className='signupButton homeSignUpButtonText homeSignUpButton' onClick={this.handleSubmit}>Sign Up</button>
-        </div>
 
-        {displayError}
-        <Link className='loginHomeButton' to='/'>Home</Link>  
+        <div className='loginRight signupRight'>
+          {displayRequiredInputs}
+          {displayJoiningInput}
+          {displayFirebaseError}
+          {displayRequiredError}
+
+          <button type="submit" className='loginButton loginButtonText signupButton' onClick={this.handleSubmit}>Sign Up</button>
+        </div>
+        
       </div>
-  </div>
+    </div>
 
     return (
       <AuthContext.Consumer>
@@ -87,7 +131,7 @@ export default class Signup extends React.Component {
           }
         }
       </AuthContext.Consumer>
-      
+
     );
   }
 }
