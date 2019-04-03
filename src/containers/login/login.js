88d9cb517e9d1c81601f8a2b58eab0b3b36bb51a
Pamela Abreu <43766342+pamelaabreu@users.noreply.github.com>
2019-04-03 16:29:20 -0400

    Axios Request for Login (#17)
    * Updated whitespace around AuthContext.Provider value
    
    * Wrapped emojiis in span tags
    
    * Imported axios
    
    * Created axios get request to get id of user in db
    
    * Updated the app wide dbUid by using context function

--- src/containers/login/login.js
+++ src/containers/login/login.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import firebase from '../../firebase';
+import axios from 'axios';
 import AuthContext from '../../contexts/auth';
 import { Redirect, Link } from 'react-router-dom';
 import './login.css';
 
 export default class Login extends React.Component {
+  static contextType = AuthContext;
 
   state = {
     email: '',
@@ -21,9 +23,11 @@ export default class Login extends React.Component {
 
     const { email, password } = this.state;
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then((response) => {
-        console.log('Returns: ', response);
+      .then(response => response.user.uid)
+      .then(uid => {
+        return axios.get(`http://localhost:3000/login/${uid}`)
       })
+      .then(res => this.context.updateDbUid(res.data.id))
       .catch(err => {
         const { message } = err;
         this.setState({ error: message });
@@ -33,22 +37,22 @@ export default class Login extends React.Component {
 
   render() {
     const { email, password, error } = this.state;
-    const displayError = error === '' ? '' : <p className='loginError' role="alert">😞 {error}</p>
+    const displayError = error === '' ? '' : <p className='loginError' role="alert"><span role='img' aria-label="Sad Face">😞</span> {error}</p>
     const displayForm = <div className='loginBackground'>
      <div className='loginBox'>
         <div className='loginLeft'>
           <h1 className='loginTitle'>Login</h1>
         </div>
         <div className='loginRight'>
-          {displayError}
             <div className='loginFlex'>
-                <label className='loginInputTitle'>✉️</label>
+                <label className='loginInputTitle'><span role='img' aria-label="email">✉️</span></label>
                 <input className='loginInput' type="email" placeholder="Enter email" name="email" value={email} onChange={this.handleChange} />
             </div>
             <div className='loginFlex'>
-                <label className='loginInputTitle'>🔑</label>
+                <label className='loginInputTitle'><span role='img' aria-label="password">🔑</span></label>
                 <input className='loginInput' type="password" placeholder="Password" value={password} name="password" onChange={this.handleChange} />
             </div>
+            {displayError}
             <p className='loginButton loginButtonText' type="submit" onClick={this.handleSubmit}>Login</p>
         </div>
       </div>
