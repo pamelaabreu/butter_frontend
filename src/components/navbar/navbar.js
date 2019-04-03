6dfafd6308ffbbb02b1f07d9b217c3793503f5d0
Pamela Abreu <pamelaabreu@pursuit.com>
2019-04-03 19:48:42 -0400

    Changed component into a class, declared static contextType, and changed the Link to the createPost

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,45 +3,50 @@ import { Link } from 'react-router-dom';
 import AuthContext from '../../contexts/auth';
 import './navbar.css';
 
-export default (props) => {
-    const navbar = <>
-        <div className="navbarBox navbarTop">
-            <div className="navbarButton newsfeed">
-                <Link className='navbarText newsfeed' to="/">h</Link>
-            </div>
-        
-            <div className="navbarButton explore">
-                <Link className='navbarText explore' to="/explore">e</Link>
-            </div>
-        
-            <div className="navbarButton search">
-                <Link className='navbarText search' to="/search">s</Link>
-            </div>
-        </div>
+export default class Navbar extends React.Component {
+    static contextType = AuthContext;
 
-        <div className="navbarBox navbarBottom">
-            <div className="navbarButton favorites">
-                <Link className='navbarText favorites' to="/favorites/:id">b</Link>
-            </div>
-        
-            <div className="navbarButton user">
-                <Link className='navbarText user' to="/user/:id">u</Link>
-            </div>
-        
-            <div className="navbarButton plus">
-                <Link className='navbarText plus' to="/createPost/:id">+</Link>
+    render () {
+
+        const navbar = <>
+            <div className="navbarBox navbarTop">
+                <div className="navbarButton newsfeed">
+                    <Link className='navbarText newsfeed' to="/">h</Link>
+                </div>
+            
+                <div className="navbarButton explore">
+                    <Link className='navbarText explore' to="/explore">e</Link>
+                </div>
+            
+                <div className="navbarButton search">
+                    <Link className='navbarText search' to="/search">s</Link>
+                </div>
             </div>
 
-            <div className="navbarButton notification">
-                <Link className='navbarText notification' to="/notifications/:id">n</Link>
+            <div className="navbarBox navbarBottom">
+                <div className="navbarButton favorites">
+                    <Link className='navbarText favorites' to="/favorites/:id">b</Link>
+                </div>
+            
+                <div className="navbarButton user">
+                    <Link className='navbarText user' to="/user/:id">u</Link>
+                </div>
+            
+                <div className="navbarButton plus">
+                    <Link className='navbarText plus' to={'/createPost/'+this.context.firebaseUid}>+</Link>
+                </div>
+
+                <div className="navbarButton notification">
+                    <Link className='navbarText notification' to="/notifications/:id">n</Link>
+                </div>
             </div>
-        </div>
-    </>
-  return (
-        <AuthContext.Consumer>
-            {
-                ({user}) =>  user ? navbar : null
-            }
-        </AuthContext.Consumer>
-  )
+        </>
+        return (
+                <AuthContext.Consumer>
+                    {
+                        ({user}) =>  user ? navbar : null
+                    }
+                </AuthContext.Consumer>
+        )
+    }
 }
