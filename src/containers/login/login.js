715b3229bd68787ce274bf25733c22255d28b825
Pamela Abreu <pamelaabreu@pursuit.com>
2019-04-03 01:53:53 -0400

    Wrapped emojiis in span tags

--- src/containers/login/login.js
+++ src/containers/login/login.js
@@ -33,22 +33,22 @@ export default class Login extends React.Component {
 
   render() {
     const { email, password, error } = this.state;
-    const displayError = error === '' ? '' : <p className='loginError' role="alert">😞 {error}</p>
+    const displayError = error === '' ? '' : <p className='loginError' role="alert"><span role='img' aria-label="Sad Face">😞</span> {error}</p>
     const displayForm = <div className='loginBackground'>
      <div className='loginBox'>
         <div className='loginLeft'>
           <h1 className='loginTitle'>Login</h1>
         </div>
         <div className='loginRight'>
-          {displayError}
             <div className='loginFlex'>
-                <label className='loginInputTitle'>✉️</label>
+                <label className='loginInputTitle'><span role='img' aria-label="email">✉️</span></label>
                 <input className='loginInput' type="email" placeholder="Enter email" name="email" value={email} onChange={this.handleChange} />
             </div>
             <div className='loginFlex'>
-                <label className='loginInputTitle'>🔑</label>
+                <label className='loginInputTitle'><span role='img' aria-label="password">🔑</span></label>
                 <input className='loginInput' type="password" placeholder="Password" value={password} name="password" onChange={this.handleChange} />
             </div>
+            {displayError}
             <p className='loginButton loginButtonText' type="submit" onClick={this.handleSubmit}>Login</p>
         </div>
       </div>
