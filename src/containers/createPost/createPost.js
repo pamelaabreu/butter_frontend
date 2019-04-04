3007b3f07cf9bd064062dbcfca00a8093150a3c8
Pamela Abreu <pamelaabreu@pursuit.com>
2019-04-03 20:22:29 -0400

    Updated state to have content_url, title, tags, tag_id, error; Created handleFileInput and HandleChange functions, created JSX for displaying the imgInput, titleInput

--- src/containers/createPost/createPost.js
+++ src/containers/createPost/createPost.js
@@ -2,19 +2,56 @@ import React from 'react';
 import firebase from '../../firebase';
 import axios from 'axios';
 import AuthContext from '../../contexts/auth';
+import { Redirect } from 'react-router-dom';
+import './createPost.css';
 
-export default class createPost extends React.Component {
+export default class CreatePost extends React.Component {
+
+    state = {
+        content_url:{},
+        title:'',
+        tags:[],
+        tag_id: '',
+        error: ''
+    }
+
+    handleFileInput = e => this.setState({ [e.target.name]: e.target.files[0] })
+
+    handleChange = e => this.setState({ [e.target.name]: e.target.value.trim() })
+
+    componentDidMount () {
+        axios.get('')
+    }
+
     render () {
-        <AuthContext.Consumer>
-            {
-                ({user}) => {
-                    if(user){
-                        return <h1>Create Post</h1>
-                    } else {
-                        return <Redirect to='/' />
+        const displayProfileImgInput = <div className='createPostFlex'>
+            <label className='createPostInputTitle'>Post Image</label>
+            <input className='createPostInput createPostInputFile' type='file' name='content_url' onChange={this.handleFileInput} />
+        </div>
+        const displayTitleInput = <div className='createPostFlex'>
+            <label className='createPostInputTitle'>Title</label>
+            <input className='createPostInput createPostInputFile' type='text' name='title' onChange={this.handleChange} />
+        </div>
+        const displayForm = <div className='createPostBox'>
+            {displayProfileImgInput}
+            {displayTitleInput}
+            <div>
+                <h2>Tags</h2>
+            </div>
+        </div>
+
+        return(
+            <AuthContext.Consumer>
+                {
+                    ({user}) => {
+                        if(user){
+                            return displayForm;
+                        } else {
+                            return <Redirect to='/' />
+                        }
                     }
                 }
-            }
-        </AuthContext.Consumer>
+            </AuthContext.Consumer>
+        );   
     }
 }
