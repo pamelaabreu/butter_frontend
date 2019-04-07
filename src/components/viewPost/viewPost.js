532c19ed4c9099152e43cbbef61491fba23a7da6
Pamela Abreu <pamelaabreu@pursuit.com>
2019-04-07 12:50:11 -0400

    Imported css for ViewPost and organized JSX

--- src/components/viewPost/viewPost.js
+++ src/components/viewPost/viewPost.js
@@ -2,21 +2,44 @@ import React from 'react';
 import AuthContext from '../../contexts/auth';
 import { Redirect } from 'react-router-dom';
 import axios from 'axios';
+import PostService from '../../services/post';
+import './viewPost.css';
 
 export default class ViewPost extends React.Component {
+    static contextType = AuthContext;
+
+    state = {
+        postInfo: null,
+        commentsInfo: null,
+        likesInfo: null,
+        userInfo: null,
+        tagInfo: null,
+        error: null
+    }
 
     componentDidMount () {
-        axios.get(`http://localhost:3000/post/${parseInt(this.props.match.params.id)}/`)
-        .then(res => console.log(res.data))
+
+        PostService.getPostInformation(this.props.match.params.id)
+        .then(({postInfo, likesInfo, commentsInfo, userInfo, tagInfo}) => this.setState({postInfo, likesInfo, commentsInfo, userInfo, tagInfo}))
+        .catch(err => this.setState({ error: 'Trouble getting post information. Please try again later!' }))
     }
 
     render(){
+        const {postInfo, likesInfo, commentsInfo, userInfo, tagInfo, error} = this.state;
+
+        const displayError = error ? <h1>{error}</h1> : null;
+        const displayPost = <>
+            <div className='viewPostBox'>
+                {displayError}
+                <h1>Hellow View Post! </h1> 
+            </div>
+        </>
 
        return (
         <AuthContext.Consumer>
             {
                 
-                ({user}) => user ? <h1>Hellow View Post! </h1> : <Redirect to='/' />
+                ({user}) => user ? displayPost : <Redirect to='/' />
                 
             }
         </AuthContext.Consumer>
