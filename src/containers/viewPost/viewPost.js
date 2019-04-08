0243f8642add35cc15c1fc810266b7d5244aba16
Pamela Abreu <pamelaabreu@pursuit.com>
2019-04-07 20:56:18 -0400

    Created dropdown for tag img and moved around JSX

--- src/containers/viewPost/viewPost.js
+++ src/containers/viewPost/viewPost.js
@@ -35,20 +35,26 @@ export default class ViewPost extends React.Component {
                 <div className='viewPostImageContainer'>
                     <img className='img-fluid' alt={title} src={content_url} />
                     {
-                        tagInfo ? <img className='viewPostTagContainer' alt={tagInfo.topic_name} src={tagInfo.image_url} /> : null
+                        tagInfo ? 
+                        <>
+                            <img className='viewPostTagContainer' alt={tagInfo.topic_name} src={tagInfo.image_url} /> 
+                            <h3 className='viewPostTagDropdown'>{tagInfo.topic_name}</h3>
+                        </>
+                        : 
+                        null
                     }
-                </div>
-                <div className='viewPostInfoContainer'>
                     <div className='viewPostUsername'>
                         <Link className='viewPostUsername' to={'/user/'+username}>@{username}</Link>
                     </div>
+                    <h3 className='viewPostCaption'>"{caption}"</h3>
+                </div>
+                <div className='viewPostInfoContainer'>
                     <h2 className='viewPostTitle'>{title}</h2>
-                    <h3 className='viewPostCaption'>{caption}</h3>
                     <p>{likesInfo.length} YAS!</p>
                     <p>{summary}</p>
-                    <h3>{moment(created_at).format('LL')}</h3>
-                    <h3>{moment(created_at).format('LT')}</h3>
                     <p>View all {commentsInfo.length} comments</p>
+                    <h3 className='viewPostDate'>{moment(created_at).format('LL')}</h3>
+                    <h3 className='viewPostDate'>{moment(created_at).format('LT')}</h3>
                 </div>
                 
             </div>
