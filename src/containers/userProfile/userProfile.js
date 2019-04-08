5cb57d3683a7c0fc4b7572f48cd046f59bd33d69
Pamela Abreu <pamelaabreu@pursuit.com>
2019-04-08 11:45:29 -0400

    Created basic showing of userInformation like profile picture, username, and followers/followings numbers

--- src/containers/userProfile/userProfile.js
+++ src/containers/userProfile/userProfile.js
@@ -8,23 +8,39 @@ export default class UserProfile extends React.Component {
         userInfo:{},
         userFollowers: [],
         userFollowings: [],
-        userPosts: []
+        userPosts: [],
+        error: null
     }
 
     componentDidMount () {
         UserService.getUsersInfo(this.props.match.params.id)
         .then(({userInfo, userFollowers, userFollowings, userPosts}) => this.setState({userInfo, userFollowers, userFollowings, userPosts}))
+        .catch(err => this.setState({ userInfo:{}, userFollowers:{}, userFollowings:{}, userPosts:{}, error:`Trouble getting ${this.props.match.params.id}. Please try again later.` }))
+    }
+
+    getHoroscopeSign = (birthday) => {
+        const dates = birthday.split('-')
     }
 
     render () {
+        const {userInfo, userFollowers, userFollowings, userPosts} = this.state;
+
+        const displayUserProfile = <div>
+            <div className='userInfoContainer'>
+                <div style={{width:'60px', height:'60px', backgroundSize:'cover', backgroundPosition:'center', backgroundImage:"url(" + userInfo.profile_img + ")"}}></div>
+                <h2>{userInfo.username}</h2>
+                <h2>{userFollowers.length} Followers</h2>
+                <h2>{userFollowings.length} Following</h2>
+                <Link style={{border:'2px solid rebeccapurple', padding:'20px'}} to='/logout'>Logout</Link> 
+            </div>
+        </div>
+
         return (
             <AuthContext.Consumer>
                 {
                     ({user}) => user ? <>
                         <div style={{marginTop:'100px'}}>
-                            <h2>Hello, {user.email}</h2>
-                            <h4>With ID:{user.uid} </h4>
-                            <Link style={{border:'2px solid rebeccapurple', padding:'20px'}} to='/logout'>Logout</Link> 
+                            {displayUserProfile}
                         </div>
                     </>
                     : 
