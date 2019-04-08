58282e0d52c4b0a1880d76a81eb8c0659ece99f8
Pamela Abreu <43766342+pamelaabreu@users.noreply.github.com>
2019-04-08 16:17:30 -0400

    User Profile Component (#22)
    * Created basic showing of userInformation like profile picture, username, and followers/followings numbers
    
    * Created ternary operator to display follow placeholder button
    
    * Added function call to convert birthday into horoscope sign
    
    * Created getHoroscopeSign that returns sign name
    
    * Added styling for the userProfile component
    
    * Added styling for JSX and created JSX for the post images

--- src/containers/userProfile/userProfile.js
+++ src/containers/userProfile/userProfile.js
@@ -2,33 +2,57 @@ import React from 'react';
 import { Redirect, Link } from 'react-router-dom';
 import AuthContext from '../../contexts/auth';
 import UserService from '../../services/user';
+import './userProfile.css';
 
 export default class UserProfile extends React.Component {
+    static contextType = AuthContext;
+
     state = {
-        userInfo:{},
+        userInfo:{
+            birthday:''
+        },
         userFollowers: [],
         userFollowings: [],
-        userPosts: []
+        userPosts: [],
+        error: null
     }
 
     componentDidMount () {
         UserService.getUsersInfo(this.props.match.params.id)
         .then(({userInfo, userFollowers, userFollowings, userPosts}) => this.setState({userInfo, userFollowers, userFollowings, userPosts}))
+        .catch(err => this.setState({ userInfo:{}, userFollowers:{}, userFollowings:{}, userPosts:{}, error:`Trouble getting ${this.props.match.params.id}. Please try again later.` }))
     }
 
     render () {
+        const {userInfo, userFollowers, userFollowings, userPosts} = this.state;
+        const sign = UserService.getHoroscopeSign(userInfo.birthday);
+
+        const displayPosts = userPosts.map((e, i) => {
+            return <Link to={"/viewPost/" + e.id}>
+                    <div key={i} className='userProfilePosts' style={{backgroundImage:"url(" + e.content_url + ")"}}></div>
+                </Link>
+        });
+
+        const displayUserProfile = <div className='userProfileContainer'>
+            <div className='userContainer'>
+                <div className='userInfoContainer'>
+                    <div className='userProfileImg' style={{backgroundImage:"url(" + userInfo.profile_img + ")"}}></div>
+                    <h2 className='userInfoText'>{userInfo.username}</h2>
+                    <h2 className='userInfoText'>{sign.toLowerCase()}</h2>
+                    <h2 className='userInfoText'>{userFollowers.length} followers / {userFollowings.length} following</h2>
+                </div>
+                
+                {userInfo.id === this.context.dbUid ? null : <h2>Follow Placeholder</h2>}
+                {userInfo.id === this.context.dbUid ? <div className='userLogoutContainer'><Link className='userInfoText userLogout' to='/logout'>l</Link></div> : null}
+                
+            </div>
+            {displayPosts}
+        </div>
+
         return (
             <AuthContext.Consumer>
                 {
-                    ({user}) => user ? <>
-                        <div style={{marginTop:'100px'}}>
-                            <h2>Hello, {user.email}</h2>
-                            <h4>With ID:{user.uid} </h4>
-                            <Link style={{border:'2px solid rebeccapurple', padding:'20px'}} to='/logout'>Logout</Link> 
-                        </div>
-                    </>
-                    : 
-                    <Redirect to='/'/>
+                    ({user}) => user ? displayUserProfile : <Redirect to='/'/>
                 }
             </AuthContext.Consumer>
         )
