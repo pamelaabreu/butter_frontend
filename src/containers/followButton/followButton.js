b221a0402f0118daeb5996767551a8331ae0c729
Pamela Abreu <pamelaabreu@pursuit.com>
2019-04-09 01:28:17 -0400

    Imported FollowService to be invoked in the componentDidMount to setState the following, userFollowerId, userFollowingId, and dbFollowingId

--- src/containers/followButton/followButton.js
+++ src/containers/followButton/followButton.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import AuthContext from '../../contexts/auth';
+import FollowService from '../../services/follow';
 
 export default class FollowButton extends React.Component {
+    static contextType = AuthContext;
+
+    state = {
+        following: null,
+        userFollowerId: null,
+        userFollowingId: null,
+        dbFollowId: null,
+    }
+
+    componentDidMount () {
+        FollowService.checkUserFollowing(this.context.dbUid, this.props.username)
+        .then(({ userFollowerId, userFollowingId, dbFollowId, following }) => this.setState({ userFollowerId, userFollowingId, dbFollowId, following }))
+    }
+
     render () {
+        // console.log(this.props.username)
+        // console.log(this.context.dbUid)
         return (
             <button>Follow</button>
         );
