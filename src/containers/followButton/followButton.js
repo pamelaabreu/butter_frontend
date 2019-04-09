7fb694364afe84ba0ea203d1aedcc3ac6f32bb4a
Pamela Abreu <43766342+pamelaabreu@users.noreply.github.com>
2019-04-09 15:40:37 -0400

    Follow Button Component (#24)
    * Created intial setup for follow button
    
    * Created follow service file that has checkUserFollowing function
    
    * Imported FollowService to be invoked in the componentDidMount to setState the following, userFollowerId, userFollowingId, and dbFollowingId
    
    * Created service function to make a POST request when a user follows another user
    
    * Created conditional statement to render a unfollow button or follow button and gave each button an onClick; On follow click, used FollowService function to make POST request and setState based on the response
    
    * Create deleteFollow DELETE request
    
    * Invoked FollowService.deleteFollow to make a DELETE request and setState following and dbFollowId
    
    * Imported Follow Button component
    
    * Created styling for followButton
    
    * Imported followButton css and added classNames to buttons
    
    * Created media query for when the screen reaches max-width of 1176px
    
    * Added background gradient animation on hover and media query for max-width of 1176px
    
    * Added styling to override default google styling on input field when autofilling
    
    * Changed the font-size on the followButton
    
    * Imported FollowButton and passed username through it's prop

---
+++ src/containers/followButton/followButton.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import AuthContext from '../../contexts/auth';
+import FollowService from '../../services/follow';
+import './followButton.css';
+
+export default class FollowButton extends React.Component {
+    static contextType = AuthContext;
+
+    state = {
+        following: null,
+        userFollowerId: null,
+        userFollowingId: null,
+        dbFollowId: null,
+        error: null
+    }
+
+    handleFollow = () => {
+        const { userFollowerId,userFollowingId } = this.state;
+
+        FollowService.createFollow(userFollowerId,userFollowingId)
+        .then(dbFollowId => this.setState({ dbFollowId, following: true }))
+        .catch(err => this.setState({ error: `Trouble following ${this.props.username}` }))
+        
+    }
+
+    handleUnfollow = () => {
+        FollowService.deleteFollow(this.state.dbFollowId)
+        .then(data => this.setState({ following: false, dbFollowId: null }))
+        .catch(err => this.setState({ error: `Trouble unfollowing ${this.props.username}` }))
+    }
+
+    componentDidMount () {
+        FollowService.checkUserFollowing(this.context.dbUid, this.props.username)
+        .then(({ userFollowerId, userFollowingId, dbFollowId, following }) => this.setState({ userFollowerId, userFollowingId, dbFollowId, following }))
+    }
+
+    render () {
+        if(this.state.following){
+            return <button className='followButton' onClick={this.handleUnfollow}>Unfollow</button>
+        } else return <button className='followButton' onClick={this.handleFollow}>Follow</button>
+          
+    }
+}
---
+++ src/services/follow.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+const FollowService = {};
+
+FollowService.checkUserFollowing = (userFollowerId, userFollowingName) => {
+    const getUserFollowingId = axios.get(`http://localhost:3000/user/username/${userFollowingName}/`).then(res => res.data.id)
+    
+    const checkFollowing = getUserFollowingId.then(userFollowingId => axios.get(`http://localhost:3000/follow/checkFollowing/${userFollowerId}/${userFollowingId}`).then(res => res.data))
+
+    return Promise.all([getUserFollowingId, checkFollowing]).then(allData => {
+        const userFollowingId = allData[0];
+        const followData = allData[1];
+
+        if(followData){
+            return { userFollowerId, userFollowingId, dbFollowId: followData.id, following: true }
+            
+        } else {
+            return { userFollowerId, userFollowingId, dbFollowId: null, followData:{}, following: false }
+        }
+    })
+}
+
+FollowService.createFollow = (userFollowerId, userFollowingId) => {
+    return axios.post('http://localhost:3000/follow/', { user_follower_id: userFollowerId, user_following_id: userFollowingId })
+    .then(res => res.data.followId)
+}
+
+FollowService.deleteFollow = dbFollowId => {
+    return axios.delete(`http://localhost:3000/follow/${dbFollowId}`)
+    .then(res => res.data)
+}
+
+export default FollowService;
