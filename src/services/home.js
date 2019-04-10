4498660067b4a977881c43f8c9806729345a8304
Pamela Abreu <pamelaabreu@pursuit.com>
2019-04-10 17:05:21 -0400

    Modified function to make GET request for each post the user's information and send back an object holding the post and user info

--- src/services/home.js
+++ src/services/home.js
@@ -19,6 +19,21 @@ HomeService.getAllFollowingsPosts = (dbUid) => {
     return getFollowingUsersPosts;
   }
 
-HomeService.getAllPosts = () => axios.get(`http://localhost:3000/post/allPosts`).then(({data}) => data.slice(0).reverse())
+HomeService.getAllPosts = () => {
+ const allPosts = axios.get(`http://localhost:3000/post/allPosts`).then(({data}) => data.slice(0).reverse())
+
+ const getUsernameForPosts = allPosts.then(posts => {
+   return posts.map(e => {
+     return axios.get(`http://localhost:3000/user/${e.user_posted_id}`).then(res => {
+       const userInfo = res.data;
+       const postInfo = e;
+       return { postInfo, userInfo }
+     })
+   })
+ })
+ .then(postsWithUserInfo => Promise.all(postsWithUserInfo))
+
+ return getUsernameForPosts;
+}
 
 export default HomeService;
