7d523eaebab505c4b1e6b8634a38e1179715bf1c
Pamela Abreu <pamelaabreu@pursuit.com>
2019-04-10 17:42:20 -0400

    Imported LikeService and use function to make A GET request to setState the likes, and in the render function returned the length of the array as number of likes through JSX

--- src/containers/likeButton/likeButton.js
+++ src/containers/likeButton/likeButton.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import LikeService from '../../services/like';
 
 export default class LikeButton extends React.Component {
+    state = {
+        likes : [],
+        isLiked :  null,
+        postId : null,
+        userId : null,
+        dbLikeId : null    
+    }
+
+    componentDidMount () {
+        LikeService.getAllLikes(this.props.postInfo.id)
+        .then(likes => this.setState({ likes }))
+    }
+
     render () {
-        console.log(this.props)
         return (
             <div>
                 <button>Like</button>
-                <p>#likes yas!</p>
+                <p>{this.state.likes.length} YAS!</p>
             </div>
         );
     }
