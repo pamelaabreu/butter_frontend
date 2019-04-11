80e552a3520ca9953b498fd5f19c7e37025fce3e
Pamela Abreu <pamelaabreu@pursuit.com>
2019-04-11 16:06:38 -0400

    Filter through the data to return only notifications that was liked,commented, or followed by a different user

--- src/containers/notifications/notifications.js
+++ src/containers/notifications/notifications.js
@@ -12,35 +12,47 @@ export default class Notifications extends React.Component {
 
     componentDidMount () {
         NotificationService.getNotifs(this.context.dbUid)
-        .then(notifs => this.setState({ notifs }))
+        .then(data => {
+            const notifs = data.filter(e => {
+                if(e.user_action_id !== this.context.dbUid){
+                    return e;
+                }
+            })
+
+            this.setState({ notifs })
+
+        })
     }
 
     render () {
         const displayNotifs = this.state.notifs.map((e, i) => {
             if(e.notification_type.toLowerCase() === 'liked'){
-                return <div>
+                return <div key={i}>
                     <p>{e.username} {e.notification_type} your photo.</p>
                     <div className='userProfileImg' style={{backgroundImage:"url(" + e.content_url + ")"}}></div>
                 </div>
             } else if (e.notification_type.toLowerCase() === 'commented'){
-                return <div>
+                return <div key={i}>
                 <p>{e.username} {e.notification_type} on your photo.</p>
                 <div className='userProfileImg' style={{backgroundImage:"url(" + e.content_url + ")"}}></div>
             </div>
             } else if (e.notification_type.toLowerCase() === 'followed'){
-                return <div>
+                return <div key={i}> 
                 <p>{e.username} {e.notification_type} followed you.</p>
             </div>
             }
         })
+        
         return (
             <AuthContext.Consumer>
         {
           ({user}) => {
             if (user) {
               return <>
-              <h1>{this.context.dbUid}</h1>
-              {displayNotifs}
+              {
+                  this.state.notifs.length > 0 ? displayNotifs : <h1>No Notifications.</h1>
+              }
+              
             </>
             } else {
               return <Redirect  to='/'/>
