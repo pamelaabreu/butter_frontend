5a3118a4817259dc9935dfc4eb259bef734b2b18
Pamela Abreu <pamelaabreu@pursuit.com>
2019-04-10 20:05:56 -0400

    Imported AuthContext and setState in PromiseAll if GET request is successful

--- src/containers/likeButton/likeButton.js
+++ src/containers/likeButton/likeButton.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import AuthContext from '../../contexts/auth';
 import LikeService from '../../services/like';
 
+
 export default class LikeButton extends React.Component {
+    static contextType = AuthContext;
+
     state = {
         likes : [],
         isLiked :  null,
@@ -10,15 +14,49 @@ export default class LikeButton extends React.Component {
         dbLikeId : null    
     }
 
+    createLike = () => {
+        console.log("Here")
+    }
+
     componentDidMount () {
-        LikeService.getAllLikes(this.props.postInfo.id)
-        .then(likes => this.setState({ likes }))
+        const getLikes = LikeService.getAllLikes(this.props.postInfo.id)
+        const getIsLike = LikeService.checkUserLike(this.props.postInfo.id, this.context.dbUid)
+
+        Promise.all([getLikes, getIsLike]).then(allData => {
+            const likes = allData[0];
+            const likedInfo = allData[1];
+            if(likedInfo){
+                this.setState(
+                    { 
+                    likes, 
+                    postId: this.props.postInfo.id,
+                    userId: this.context.dbUid, 
+                    isLiked: true, 
+                    dbLikeId: likedInfo.id 
+                })
+            } else {
+                this.setState(
+                    { 
+                    likes,
+                    postId: this.props.postInfo.id,
+                    userId: this.context.dbUid, 
+                    isLiked: null, 
+                    dbLikeId: null 
+                })
+            }
+        })
+
     }
 
     render () {
         return (
             <div>
-                <button>Like</button>
+                {
+                    this.state.isLiked ? 
+                    <button >Unlike</button> 
+                    : 
+                    <button onClick={this.createLike}>Like</button>
+                }
                 <p>{this.state.likes.length} YAS!</p>
             </div>
         );
