1422b1af6d8f4dba99e8d3cc54ba9aa63a9f90c2
Pamela Abreu <pamelaabreu@pursuit.com>
2019-04-11 00:53:11 -0400

    Organized and cleaned some code

--- src/components/newsfeed/newsfeed.js
+++ src/components/newsfeed/newsfeed.js
@@ -1,23 +1,27 @@
+// REACT
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+// CSS
 import './newsfeed.css';
 
+// Components
 import LikeButton from '../../containers/likeButton/likeButton';
 
 const Newsfeed = (props) => {
     return props.allPosts.map((e, i) => {
         const {caption, content_url, id, title} = e.postInfo;
+
         return (
             <div key={i} className="newsfeedPost">
               <p className="feedTitle">{title}</p>
-              <Link to={"/video/"+ id}>
+              <Link to={"/viewPost/"+ id}>
                 <img className='feedImg' alt={title} src={content_url} />
               </Link>
                       
               <div className='newsfeedInfo'>
                 <Link className='newsfeedUsername' to={"/user/" + e.userInfo.username}>@{e.userInfo.username}</Link>
                 <LikeButton postInfo={e.postInfo} userInfo={e.userInfo}/>
-                {/* <p className="newsfeedLikes">1000 YAS!</p> */}
               </div>
           
               <p className="feedCaption">{caption}</p>
