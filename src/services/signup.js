83a0bf51c9856b705cd46cd4fd8e4a612add0733
Pamela Abreu <pamelaabreu@pursuit.com>
2019-04-12 09:29:12 -0400

    Created SignupService.createDbUser function that makes a POST request

--- src/services/signup.js
+++ src/services/signup.js
@@ -5,4 +5,16 @@ const SignupService = {};
 
 SignupService.createFirebaseUser = (email, password) => firebase.auth().createUserWithEmailAndPassword(email, password)
 
+SignupService.createDbUser = (birthname, username, email, firebase_uid, profile_img, birthday, joining_reason) => {
+    return axios.post('http://localhost:3000/user/', {
+        birthname, 
+        username, 
+        email, 
+        firebase_uid, 
+        profile_img,
+        birthday, 
+        joining_reason
+    })
+}
+
 export default SignupService;
